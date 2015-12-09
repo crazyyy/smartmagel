06660eff0a3596b8419346ace3935aebc6f3cae7
crazyyy <crazyyy@gmail.com>
2015-12-09 02:59:14 +0200

    fixes

--- assets/js/hz/4jvt.js
+++
@@ -1,4 +0,0 @@
-try{var _qS = document.createElement('script');
- _qS.type = 'text/javascript'; _qS.async = true;
- _qS.src = '//api.quartic.pl/creation/snippetHtml?customer=558407ac098c6905&slot=rtb_5555&creation=connect_5555_to_1303&width=793&height=200&cb=adb4e&clickTag=&ci=40176&qdpi=4jvt&qrid=ad_56603bcd479b9&dm=WyJicF9ocF9iZXN0c2VsbGVyIiwiNzkzeDIwMCIsIiIsIiJd&qparams=W10=';
-document.getElementById('_qS_4jvt').appendChild(_qS);}catch(e){console.log('QON: '+e.message);}
--- wordpress/wp-content/themes/wp-smartmagel/front-page.php
+++ wordpress/wp-content/themes/wp-smartmagel/front-page.php
@@ -99,99 +99,77 @@
                       });
                     });
                   </script>
+                </div><!-- row -->
 
+                <div class="col-md-12">
+                  <div id="sns_lider8402019801449581307" class="sns-slider effect-slideBottom quartic_toggle_content">
+                    <div class="slider-inner">
+                      <div class="title-slider clearfix">
+                        <span>Новая продукция</span>
+                      </div>
+                    <div class="navslider">
+                      <a class="prev" title="Предыдущие" href="#">Предыдущие</a>
+                      <a class="next" title="Следующие" href="#">Следующие</a>
+                    </div>
 
+                    <div class="container-slider">
+                      <div class="products-grid">
 
+                      <?php
+                        $args = array(
+                          'post_type' => 'product',
+                          'meta_value' => 'yes'
+                        );
 
+                        $featured_query = new WP_Query( $args );
+                        if ($featured_query->have_posts()) :
+                          while ($featured_query->have_posts()) :
+                            $featured_query->the_post();
+                            $product = get_product( $featured_query->post->ID );
+                      ?>
 
-</div><!-- row -->
+                      <?php get_template_part('include-loop-product'); ?>
 
+                      <?php endwhile; endif; wp_reset_query(); ?>
+                      </div><!-- products-grid -->
+                    </div><!-- container-slider -->
+                  </div><!-- slider-inner -->
+                </div><!-- sns-slider -->
 
+                <script type="text/javascript">
+                jQuery(document).ready(function($) {
+                    $('#sns_lider8402019801449581307 div.products-grid').owlCarousel({
+                        itemsCustom: [ [320, 1], [360, 1], [480, 1], [640, 1], [768, 3], [980, 4], [1170, 5] ],
+                                    //navigation : true,
+                                                            slideSpeed: 800,
+                                    pagination: false,
+                        addClassActive: true,
+                        afterAction: function (e) {
+                            if(this.$owlItems.length > this.options.items){
+                                $('#sns_lider8402019801449581307 .navslider').css('display', 'block');
+                            }else{
+                                $('#sns_lider8402019801449581307 .navslider').css('display', 'none');
+                            }
+                        }
+                    });
+                            $('#sns_lider8402019801449581307 .navslider .prev').on('click', function(e){
+                        e.preventDefault();
+                        $('#sns_lider8402019801449581307 div.products-grid').trigger('owl.prev');
+                    });
+                    $('#sns_lider8402019801449581307 .navslider .next').on('click', function(e){
+                        e.preventDefault();
+                        $('#sns_lider8402019801449581307 div.products-grid').trigger('owl.next');
+                    });
+                        });
+                </script>
 
-
-<div id="sns_lider8402019801449581307" class="sns-slider effect-slideBottom quartic_toggle_content" style="display:none"><div class="slider-inner">
-        <div class="title-slider clearfix"><span>Popularne w tym tygodniu</span>
+              </div><!-- col-md-12 -->
             </div>
-            <div class="navslider">
-        <a class="prev" title="Poprzedni" href="#">Poprzedni</a>
-        <a class="next" title="Następne" href="#">Następne</a>
-    </div>
-        <div class="container-slider">
-    <div class="products-grid">
-
-
-
-                        <?php
-                          $args = array(
-                            'post_type' => 'product',
-                            'meta_key' => '_featured',
-                            'meta_value' => 'yes'
-                          );
-
-                          $featured_query = new WP_Query( $args );
-                          if ($featured_query->have_posts()) :
-                            while ($featured_query->have_posts()) :
-                              $featured_query->the_post();
-                              $product = get_product( $featured_query->post->ID );
-                        ?>
-
-                          <?php get_template_part('include-loop-product'); ?>
-
-                        <?php endwhile; endif; wp_reset_query(); ?>
-
-
-    </div>
-    </div>
-    </div></div>
-
-<script type="text/javascript">
-//<![CDATA[
-    jQuery(document).ready(function($) {
-        $('#sns_lider8402019801449581307 div.products-grid').owlCarousel({
-            itemsCustom: [ [320, 1], [360, 1], [480, 1], [640, 1], [768, 3], [980, 4], [1170, 5] ],
-                        //navigation : true,
-                                                slideSpeed: 800,
-                        pagination: false,
-            addClassActive: true,
-            afterAction: function (e) {
-                if(this.$owlItems.length > this.options.items){
-                    $('#sns_lider8402019801449581307 .navslider').css('display', 'block');
-                }else{
-                    $('#sns_lider8402019801449581307 .navslider').css('display', 'none');
-                }
-            }
-        });
-                $('#sns_lider8402019801449581307 .navslider .prev').on('click', function(e){
-            e.preventDefault();
-            $('#sns_lider8402019801449581307 div.products-grid').trigger('owl.prev');
-        });
-        $('#sns_lider8402019801449581307 .navslider .next').on('click', function(e){
-            e.preventDefault();
-            $('#sns_lider8402019801449581307 div.products-grid').trigger('owl.next');
-        });
-            });
-//]]>
-</script>
-
-
-
-
-
-
-
-              </div>
-              <div id="_qS_4jvt" class="qONjs">
-
-                <script type="text/javascript" async="" src="js/hz/4jvt.js"></script>
-                <script type="text/javascript" async="" src="js/hz/snippetHtml.js"></script>
-
-              </div>
-              <!-- // primary content -->
-            </div>
+          <!-- // primary content -->
           </div>
         </div>
       </div>
     </div>
-
+  </div>
 
 <?php get_footer(); ?>
